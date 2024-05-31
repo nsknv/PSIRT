0f28da80711c81b3ccca8744357df342cbfcabed
Navtej <nkathuria@nvidia.com>
2024-05-31 15:31:16 -0700

    Removing type for TOC

--- Table_Of_Contents.md
+++ Table_Of_Contents.md
@@ -2,7 +2,7 @@
 
 Below are notifications for security and privacy events within NVIDIA Open Source applications.
 
-| Date       | Type     | Subject |
-|------------|----------|---------|
-| Updated 05/29/2024 10:14 PM | N/A | [Triton Inference Server - May 2024](./bulletins/5546.md) |
-| Updated [Date and Time] | N/A | [[Title]](./bulletins/template.md) |
+| Date       | Subject |
+|------------|---------|
+| Updated 05/29/2024 10:14 PM | [Triton Inference Server - May 2024](./bulletins/5546.md) |
+| Updated [Date and Time] | [[Title]](./bulletins/template.md) |
--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 
+# Define the path to the bulletins directory
 bulletins_dir = './bulletins'
 toc_file_path = './Table_Of_Contents.md'
 
+# Start the table of contents content
 toc_content = """# Security Bulletins
 
 Below are notifications for security and privacy events within NVIDIA Open Source applications.
 
-| Date       | Type     | Subject |
-|------------|----------|---------|
+| Date       | Subject |
+|------------|---------|
 """
 
 for bulletin_file in sorted(os.listdir(bulletins_dir)):
@@ -16,17 +18,14 @@ for bulletin_file in sorted(os.listdir(bulletins_dir)):
         with open(os.path.join(bulletins_dir, bulletin_file), 'r') as file:
             lines = file.readlines()
             date = "N/A"
-            type_ = "N/A"
             subject = "N/A"
             for line in lines:
-                if line.startswith("**Updated"):
+                if "**Updated" in line:
                     date = line.split('**')[1].strip()
                 if line.startswith("# Security Bulletin:"):
                     subject = line[len("# Security Bulletin:"):].strip()
-                if line.startswith("**Severity:"):
-                    type_ = line.split('**')[1].strip()
                     
-            toc_content += f"| {date} | {type_} | [{subject}]({os.path.join(bulletins_dir, bulletin_file)}) |\n"
+            toc_content += f"| {date} | [{subject}]({os.path.join(bulletins_dir, bulletin_file)}) |\n"
 
 with open(toc_file_path, 'w') as toc_file:
     toc_file.write(toc_content)
